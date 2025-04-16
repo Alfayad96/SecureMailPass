181a899ebc90e14b5adbae7216145c5bd446da19
Mohammad Alfayad <Mohammad.alfayad96@outlook.de>
2025-04-16 11:41:19 +0200

    Update SecureMailPass.py

--- SecureMailPass.py
+++ SecureMailPass.py
@@ -13,7 +13,7 @@ password11 = ''.join(random.choice(Password1) for _ in range(16))
 
 # Hier estellen wir variablen und speichern wir unsere Email
 #und das benötigte passwort um sie in der login-Funktion zu benutzen 
-sender_email = "mn512693@gmail.com"
+sender_email = "Hier die Email Adresse"
 my_password = "Hier das Passwort"
 # und hier eingabe von benutzer-Name und seine Email adresse 
 Benutzer_name = input("geben Sie Ihr Vor un Nachname ein:")
